05282e0ef116a2213cde0ab01ac11c0110f5eeb7
Agus <aagiribar@yahoo.es>
2023-10-07 13:46:51 +0100

    CsvFileVideogameLoader.load method implemented

--- src/main/java/kata2/CsvFileVideogameLoader.java
+++ src/main/java/kata2/CsvFileVideogameLoader.java
@@ -1,10 +1,11 @@
 package kata2;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CsvFileVideogameLoader implements VideogameLoader{
-    private File file;
+    private final File file;
 
     public CsvFileVideogameLoader(File file) {
         this.file = file;
@@ -16,6 +17,34 @@ public class CsvFileVideogameLoader implements VideogameLoader{
 
     @Override
     public List<Videogame> load() {
-        return null;
+        List<Videogame> videogames = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            reader.readLine();
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                videogames.add(toVideogame(line));
+            }
+        } catch (IOException e) {}
+    }
+
+    private Videogame toVideogame(String line) {
+        return toVideogame(line.split(","));
+    }
+
+    private Videogame toVideogame(String[] fields) {
+        return new Videogame(
+                fields[0],
+                fields[1],
+                fields[2],
+                fields[3],
+                fields[4],
+                fields[5],
+                Double.parseDouble(fields[6]),
+                Double.parseDouble(fields[7]),
+                Double.parseDouble(fields[8]),
+                Double.parseDouble(fields[9]),
+                Double.parseDouble(fields[10])
+        );
     }
 }
